fe2ac0449f45d0ca485503c94781f3d5be68db91
valentin.kozlov <valentin.kozlov@kit.edu>
2022-06-02 18:17:13 +0200

    [k8s] collect deployment and config files for o3api and o3webapp

---
+++ nsupdate/nsupdate-fedcloud.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+###
+# Shell script to update Domain Name O3API / O3WEBAPP  with the right IP
+# One HAS TO provide O3API_SECRET and O3WEB_SECRET as ENV variables!
+# Optionally one can also redefine O3API_SITE and O3WEB_SITE
+###
+
+###
+# default sites for o3api and o3webapp
+o3api_site=api.o3as.fedcloud.eu
+o3web_site=web.o3as.fedcloud.eu
+###
+
+# function to check if SITE responds, if not => update IP via nsupdate service
+check_hostname()
+{
+  site=$1
+  secret=$2
+
+  response=$(curl -s -o /dev/null -w "%{http_code}"  https://${site})
+
+  if [ $response -eq 200 ]; then
+     message="is responding, do NOT update IP"
+  else
+     message=$(curl https://${site}:${secret}@nsupdate.fedcloud.eu/nic/update)
+  fi
+  echo "${site}: ${message}"
+}
+
+# one can define O3API_SITE and O3WEB_SITE via Environment Variables
+# if not defined => use default values provided above
+if [ ${#O3API_SITE} -le 1 ]; then
+  O3API_SITE=${o3api_site}
+fi
+if [ ${#O3WEB_SITE} -le 1 ]; then 
+  O3WEB_SITE=${o3web_site}
+fi
+
+# one has to define correct O3API_SECRET and O3WEB_SECRET via Environment Variables
+# if not defined => use dummy ones for proper function calling, BUT
+# nsupdate will NOT provide proper hostnames (you need right secret)!
+if [ ${#O3API_SECRET} -le 1 ]; then
+  O3API_SECRET="dummysecret"
+fi
+if [ ${#O3WEB_SECRET} -le 1 ]; then
+  O3WEB_SECRET="dummysecret"
+fi
+
+echo $(date +"%F %T %Z")
+
+check_hostname $O3API_SITE $O3API_SECRET
+check_hostname $O3WEB_SITE $O3WEB_SECRET
