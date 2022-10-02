86b9bd124f0d678c335e2a6285e8d7a84338e2e2
Valentin.Kozlov <valentin.kozlov@gmail.com>
2022-10-02 13:39:59 +0200

    [cicd] Updating script for more deployment phases: prod, stage, test

---
+++ deploy.sh
@@ -0,0 +1,165 @@
+#!/usr/bin/env bash
+
+KUBECONFIG=$HOME/.kube/config
+
+# o3as helm chart settings
+repo="o3k8s"                        # git repository name
+pv="pv-o3as"                        # PersistentVolume allocated for o3as service
+ns="o3as"                           # Namespace reserved for o3as service
+secret="o3as-secrets"               # Secrets prepared for o3as service
+pvc="o3as-pvc"                      # Name of the PersistentVolumeClaim
+helm="o3as-app"                     # Name of the Helm  o3as chart
+helm_values_prod="values.yaml"      # Values file for the Helm o3as chart
+helm_values_stage="values.yaml"     # Values file for the Helm o3as chart
+helm_values_test="values-test.yaml" # Values file for the Helm o3as chart
+
+# Defaults
+DEP_ENV_DEFAULT="test"              # default deployment environment
+helm_values=$helm_values_test       # Default Values file for the Helm o3as chart
+
+# Usage and command line parsing
+function usage()
+{
+    shopt -s xpg_echo
+    echo "Usage: $0 <options> \n
+    Options:
+    -h|--help \t This help message
+    -p|--prod \t Deploy $helm application in the production environment
+    -s|--stage \t Deploy $helm application in the staging environment
+    -t|--test \t Deploy $helm application in the test environment" 1>&2; exit 0;
+}
+
+function check_arguments()
+{
+    OPTIONS=h,p,s,t
+    LONGOPTS=help,prod,stage,test
+    # https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+    #set -o errexit -o pipefail -o noclobber -o nounset
+    set  +o nounset
+    ! getopt --test > /dev/null
+    if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
+        echo '`getopt --test` failed in this environment.'
+        exit 1
+    fi
+
+    # -use ! and PIPESTATUS to get exit code with errexit set
+    # -temporarily store output to be able to check for errors
+    # -activate quoting/enhanced mode (e.g. by writing out “--options”)
+    # -pass arguments only via   -- "$@"   to separate them correctly
+    ! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+    if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+        # e.g. return value is 1
+        #  then getopt has complained about wrong arguments to stdout
+        exit 1
+    fi
+    # read getopt’s output this way to handle the quoting right:
+    eval set -- "$PARSED"
+
+    if [ "$1" == "--" ]; then
+        echo "[INFO] No arguments provided, using defaults."
+        DEP_ENV=$DEP_ENV_DEFAULT
+    fi
+    # now enjoy the options in order and nicely split until we see --
+    while true; do
+        case "$1" in
+            -h|--help)
+                usage
+                shift
+                ;;
+            -p|--prod)
+                DEP_ENV="prod"
+                helm_values=${helm_values_prod}
+                shift
+                ;;
+            -s|--stage)
+                DEP_ENV="stage"
+                helm_values=${helm_values_test}
+                shift
+                ;;
+            -t|--test)
+                DEP_ENV="test"
+                helm_values=${helm_values_test}
+                shift
+                ;;
+            --)
+                shift
+                break
+                ;;
+            *)
+                break
+                ;;
+            esac
+        done
+}
+
+check_arguments "$0" "$@"
+echo "Deployment ENV: $DEP_ENV, values file: $helm_values"
+
+# 0. We first lint the HELM chart
+echo "[INFO] Let's first lint the HELM chart, $helm"
+cmd="helm lint ${HOME}/${repo}/$helm --values ${HOME}/${repo}/$helm/${helm_values}"
+$cmd
+if [ "$?" -gt 0 ]; then
+  echo "[FAIL] Linting $helm Helm chart failed!"
+  echo "       Command: ${cmd}"
+  exit 10
+fi
+
+# 1. Check that pv-o3as exists
+pv_status=$(kubectl get pv |grep ${pv}|cut -d' ' -f1)
+
+if [ "${pv_status}" == "${pv}" ]; then
+   echo "[OK] PersistentVolume \"$pv\" is found"
+else
+   echo "[FAIL] You have to create \"$pv\" PersistentVolume on kubernetes first!"
+   exit 11
+fi
+
+# 2. Check that o3as namespace exists
+ns_status=$(kubectl get ns |grep $ns|cut -d' ' -f1)
+
+if [ "${ns_status}" == "$ns" ]; then
+   echo "[OK] Namespace \"$ns\" is found"
+else
+   echo "[FAIL] You have to create \"$ns\" Namespace on kubernetes first!"
+   exit 12
+fi
+
+# 3. Check if o3as secret exists in o3as namespace
+secret_status=$(kubectl get secrets -n $ns |grep $secret |cut -d' ' -f1)
+if [ "${secret_status}" == "$secret" ]; then
+   echo "[OK] Secret \"$secret\" is found in the namespace $ns"
+else
+   echo "[FAIL] You have to create \"$secret\" Secret in the $ns Namespace on kubernetes first!"
+   exit 13
+fi
+
+helm_status=$(helm list -n $ns -q)
+if [ "${helm_status}" == "$helm" ]; then
+   echo "[WARNING] Helm chart \"$helm\" is already deployed. Trying to undeploy it now.."
+   status=$(helm uninstall $helm -n $ns)
+   sleep 5
+   echo $status
+   # (force) delete corresponding PVC
+   kubectl delete pvc $pvc -n $ns &
+   sleep 5
+fi
+
+# we need to patch PVC, if it is in "Terminatiing" phase. See e.g.
+# https://veducate.co.uk/kubernetes-pvc-terminating/
+# https://github.com/kubernetes/kubernetes/issues/69697
+# if "Terminating" is found, try to reset
+while [ -n "$(kubectl get pvc -n $ns |grep $pvc |grep -i Terminating)" ];
+do
+   kubectl patch pvc $pvc -n $ns -p '{"metadata":{"finalizers":null}}'
+done
+# once PVC is deleted, PV has to be unbound
+echo "[WARNING] Resetting  PersistentVolume, $pv"
+pv_status=$(kubectl patch pv $pv -p '{"spec":{"claimRef": null}}')
+echo $pv_status
+
+
+echo "[INFO] Trying to deploy Helm chart \"$helm\".."
+helm install $helm ${HOME}/${repo}/$helm \
+     --namespace $ns \
+     --values ${HOME}/${repo}/$helm/${helm_values}
